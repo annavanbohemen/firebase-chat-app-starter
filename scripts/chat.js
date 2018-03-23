649d8f8e67b158e5fae239012c008564e64bdf0e
LevNikolayevitch <steven.vanasbeck@gmail.com>
2018-03-23 15:40:09 +0100

    username-captured

---
+++ scripts/chat.js
@@ -0,0 +1,7 @@
+function submitUserName() {
+  var inputField = document.getElementById('username');
+  var newUserName = inputField.value;
+  console.log(newUserName);
+  console.log('uklkj;lkj;lk');
+
+submitUserName();
