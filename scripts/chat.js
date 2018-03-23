1652a2887668158adab4c1259432efd4e5a23447
LevNikolayevitch <steven.vanasbeck@gmail.com>
2018-03-23 16:01:09 +0100

    small-change-chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -2,6 +2,4 @@ function submitUserName() {
   var inputField = document.getElementById('username');
   var newUserName = inputField.value;
   console.log(newUserName);
-  console.log('uklkj;lkj;lk');
-
 submitUserName();
