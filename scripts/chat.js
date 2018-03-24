19bf83f6b40c4c52dd79dca5ffdc137ec0d44015
abbasalwash <abbas@alwash.nl>
2018-03-24 19:12:05 +0100

    chatapp versie 2

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,11 +1,34 @@
-function submitUserName(userData) {
-  var user = userData.name;
-  console.log(userData.name);
-  var linkElement = document.getElementById('addusers');
-  linkElement.innerText = user
+var username;
 
-  // var listItem = document.createElement('li');
-  // listItem.className = 'active-users';
-  //
-  //
+ChatApp.addMessageListener(messageHandler);
+
+function messageHandler(changeType, messageId, messageData) {
+  console.log(changeType);
+  console.log(messageId);
+  console.log(messageData);
+
+  if (changeType === 'added') {
+    var chatbox = document.getElementById('chatbox');
+    var p = document.createElement('p');
+    p.innerText = messageData.userName + ': ' + messageData.text;
+    chatbox.appendChild(p);
+  }
+};
+
+function callbackUserDatabase(userData) {
+  console.log(userData);
+  var linkElement = document.getElementById('userslist');
+  var listItem = document.createElement('li');
+  listItem.innerText = userData.name;
+  linkElement.appendChild(listItem);
 }
+
+function parseUser() {
+  username = document.getElementById('username').value;
+  ChatApp.createOrUpdateUser(username, -1, callbackUserDatabase);
+};
+
+function parseUserMessage() {
+  var userMessage = document.getElementById('usermsg').value;
+  ChatApp.newMessage(username, userMessage);
+};
