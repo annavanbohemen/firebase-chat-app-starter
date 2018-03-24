1f4f080f18dfe306c4542592402ccf9408a4778e
annavanbohemen <anna@vanbohemen.com>
2018-03-24 08:44:23 +0100

    user to firebase and callback

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,11 +1,22 @@
-function submitUserName(userData) {
-  var user = userData.name;
-  console.log(userData.name);
-  var linkElement = document.getElementById('addusers');
-  linkElement.innerText = user
+// function submitUserName(userData) {
+//   var user = userData.name;
+//   console.log(userData.name);
+//   console.log(userData);
+//   var linkElement = document.getElementById('addusers');
+//   linkElement.innerText = user
+//
+// }
 
-  // var listItem = document.createElement('li');
-  // listItem.className = 'active-users';
-  //
-  //
+function submitUserName() {
+  var name = document.getElementById('username').value;
+//  console.log(name);
+  ChatApp.createOrUpdateUser(name, -1, addUser);
 }
+
+function addUser(user) {
+  console.log(user);
+  var listName = $(`<li> ${user.name} </li>`);
+  var userList = $('#activeUsers');
+  userList.append(listName);
+
+};
