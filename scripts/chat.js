4219806155886a301fbd80358004ff4896d33a39
abbasalwash <abbas@alwash.nl>
2018-03-25 17:14:25 +0200

    color is going to db and back

--- scripts/chat.js
+++ scripts/chat.js
@@ -2,17 +2,6 @@ var username;
 var userColor;
 var activeUsers = [];
 
-// add user to the userlist LI
-// and add user to activeUsers
-function addUser(userName) {
-  var linkElement = document.getElementById('userslist');
-  var listItem = document.createElement('li');
-
-  listItem.innerText = userName;
-  linkElement.appendChild(listItem);
-  activeUsers.push(messageData.userName);
-}
-
 // add function messageHandler to ChatApp message handler
 ChatApp.addMessageListener(messageHandler);
 
@@ -25,22 +14,43 @@ function messageHandler(changeType, messageId, messageData) {
 
   if (changeType === 'added') {
     ChatApp.getUser(messageData.userName, callbackGetUser);
-    var chatbox = document.getElementById('chatbox');
-    var p = document.createElement('p');
-    p.innerText = messageData.userName + ': ' + messageData.text;
-    chatbox.appendChild(p);
-  }
 
-  if (activeUsers.indexOf(messageData.userName) < 0) {
-    addUser(messageData.userName);
+    // this function is called, when ChatApp.getUser is done
+    // userData contains username + color
+    // add the message to HTML with color if the user is not in activeUsers
+    // otherwise only the message without the color
+    function callbackGetUser(userData) {
+      if (activeUsers.indexOf(messageData.userName) < 0) {
+        // addUser(messageData.userName);
+        console.log(userData);
+
+        var chatbox = document.getElementById('chatbox');
+        var p = document.createElement('p');
+        p.innerHTML = `<p style="color:${userData.color};">${messageData.userName}: ${messageData.text}</p>`;
+        chatbox.appendChild(p);
+
+        // add user to the userlist li
+        // and add user to activeUsers
+        var linkElement = document.getElementById('userslist');
+        var listItem = document.createElement('li');
+        listItem.innerHTML = `<li style="color:${userData.color};">${messageData.userName}</li>`
+        linkElement.appendChild(listItem);
+
+        activeUsers.push(userName);
+      } else if (activeUsers.indexOf(messageData.userName) >= 0) {
+        var chatbox = document.getElementById('chatbox');
+        var p = document.createElement('p');
+        p.innerText = messageData.userName + ': ' + messageData.text;
+        chatbox.appendChild(p);
+      }
+    }
   }
 };
 
 // this function will be called when ChatApp.createOrUpdateUser is done
-// to add user in the userslist
+// it will console.log
 function callbackUserDatabase(userData) {
   console.log(userData);
-  addUser(userData.name);
 };
 
 // parse the username & usercolor from the HTML form
@@ -56,10 +66,4 @@ function parseUser() {
 function parseUserMessage() {
   var userMessage = document.getElementById('usermsg').value;
   ChatApp.newMessage(username, userMessage);
-};
-
-// this function is called, when ChatApp.getUser is done
-// userData contains username + color
-function callbackGetUser(userData) {
-  console.log(userData);
 };
